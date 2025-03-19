df08e79ce14f59fe333265faa1f2fc63b3059ca6
gamblinflanagan <gamblinflanagan97@gmail.com>
2025-03-19 15:09:27 -0400

    fix mobile menu

--- components/nav.js
+++ components/nav.js
@@ -39,9 +39,9 @@ const Nav = (props) => {
         {isMenuOpen && (
           <div className="md:hidden bg-[#0B0F19] border-b border-white/10">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              <a href="#" className="block px-3 py-2 text-gray-300 hover:text-[#33CCFF]">Skills</a>
-              <a href="#" className="block px-3 py-2 text-gray-300 hover:text-[#33CCFF]">Portfolio</a>
-              <a href="#" className="block px-3 py-2 text-gray-300 hover:text-[#33CCFF]">Contact</a>
+              <p onClick={() => props.ClickHandler('skills')} className="text-gray-300 hover:text-[#33CCFF] transition-colors cursor-pointer">Skills</p>
+              <p onClick={() => props.ClickHandler('portfolio')} className="text-gray-300 hover:text-[#33CCFF] transition-colors cursor-pointer">Portfolio</p>
+              <p onClick={() => props.ClickHandler('contact')} className="text-gray-300 hover:text-[#33CCFF] transition-colors cursor-pointer">Contact</p>
             </div>
           </div>
         )}
