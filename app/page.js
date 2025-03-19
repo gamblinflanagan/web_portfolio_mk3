369fb26bda8bf01ad0834097e9c7344f254c0a27
gamblinflanagan <gamblinflanagan97@gmail.com>
2025-03-19 18:19:51 -0400

    add dynamic imports to fix build error

--- app/page.js
+++ app/page.js
@@ -1,9 +1,45 @@
 'use client';
 
 import { useRef } from 'react';
+import dynamic from 'next/dynamic';
+
+// const Nav = dynamic(
+//   () => import('@/components/nav'),
+//   {
+//     ssr: false,
+//   }
+// );
+
+const Main = dynamic(
+  () => import('@/components/main-section'),
+  {
+    ssr: false,
+  }
+);
+
+// const Skills = dynamic(
+//   () => import('@/components/skills-section'),
+//   {
+//     ssr: false,
+//   }
+// );
+
+// const Portfolio = dynamic(
+//   () => import('@/components/portfolio-section'),
+//   {
+//     ssr: false,
+//   }
+// );
+
+// const Contact = dynamic(
+//   () => import('@/components/contact-section'),
+//   {
+//     ssr: false,
+//   }
+// );
 
 import Nav from '@/components/nav';
-import Main from '@/components/main-section';
+// import Main from '@/components/main-section';
 import Skills from '@/components/skills-section';
 import Portfolio from '@/components/portfolio-section';
 import Contact from '@/components/contact-section';
